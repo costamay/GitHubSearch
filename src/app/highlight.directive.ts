656b1f9ab46110ccc3dfa54f1307d271f122c104
Titus Ouko <titusouko@gmail.com>
2019-09-30 15:51:11 +0300

    Create custom directive

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -1,10 +1,12 @@
-import { Directive, ElementRef } from '@angular/core';
+import { HomeComponent } from './home/home.component';
+import { User } from './user';
+import { Directive, ElementRef, Input } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
-
+@Input() user: User;
   constructor(private elem:ElementRef){
     this.elem.nativeElement.style.backgroundColor='yellow';
   }
