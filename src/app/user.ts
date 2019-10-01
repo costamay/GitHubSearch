6c46e6f8af723e062cee10b7409bf5196427cafb
Titus Ouko <titusouko@gmail.com>
2019-10-01 08:41:02 +0300

    Add email property

--- src/app/user.ts
+++ src/app/user.ts
@@ -10,6 +10,9 @@ export class User {
         public created_at: Date,
         public public_repos: number,
         public html_url: any,
+        public email:any,
+        
       ){}
         
 }
+
